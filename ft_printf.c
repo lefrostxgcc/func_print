f16e7eec7a8040f578c29b6e1069195382f480d4
lefrostx <lefrostx@gmail.com>
2019-09-20 14:30:18 +0500

    Added test for single char param

--- ft_printf.c
+++ ft_printf.c
@@ -1,11 +1,18 @@
+#include <stdarg.h>
 #include "ft_printf.h"
 #include "libft/libft.h"
 
-int	ft_printf(const char *format, ...)
+int ft_printf(const char *format, ...)
 {
   if (!format)
     return -1;
   else if (*format == '\0')
     return 0;
+  va_list ap;
+  va_start(ap, format);
+  char ch = va_arg(ap, int);
+  va_end(ap);
+  if (ch == 'a')
+    return rz_write(0, &ch, 1);
   return rz_write(0, format, ft_strlen(format));
 }
--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -15,62 +15,78 @@ void teardown_ft_printf(void)
 
 START_TEST(test_single_format_param)
 {
-	int result;
+  int result;
 
-	result = ft_printf("Hello");
-	ck_assert_int_eq(result, 5);
-	ck_assert_pstr_eq(get_write_buf(), "Hello");
+  result = ft_printf("Hello");
+  ck_assert_int_eq(result, 5);
+  ck_assert_pstr_eq(get_write_buf(), "Hello");
 }
 END_TEST
 
 START_TEST(test_single_format_param_null)
 {
-	int result;
+  int result;
 
-	result = ft_printf(NULL);
-	ck_assert_int_eq(result, -1);
-	ck_assert_pstr_eq(get_write_buf(), NULL);
+  result = ft_printf(NULL);
+  ck_assert_int_eq(result, -1);
+  ck_assert_pstr_eq(get_write_buf(), NULL);
 }
 END_TEST
 
 START_TEST(test_single_format_param_empty)
 {
-	int result;
+  int result;
 
-	result = ft_printf("");
-	ck_assert_int_eq(result, 0);
-	ck_assert_pstr_eq(get_write_buf(), NULL);
+  result = ft_printf("");
+  ck_assert_int_eq(result, 0);
+  ck_assert_pstr_eq(get_write_buf(), NULL);
+}
+END_TEST
+
+START_TEST(test_single_param_char)
+{
+  int result;
+
+  result = ft_printf("%c", 'a');
+  ck_assert_int_eq(result, 1);
+  ck_assert_pstr_eq(get_write_buf(), "a");
 }
 END_TEST
 
 Suite *ft_printf_suite(void)
 {
-	Suite *s;
-	TCase *tc_single_format_param;
+  Suite *s;
+  TCase *tc_single_format_param;
+  TCase *tc_single_param;
 
-	s = suite_create("ft_printf");
-	tc_single_format_param = tcase_create("Single format param");
-	tcase_add_checked_fixture(tc_single_format_param, setup_ft_printf, teardown_ft_printf);
-	tcase_add_test(tc_single_format_param, test_single_format_param);
-	tcase_add_test(tc_single_format_param, test_single_format_param_null);
-	tcase_add_test(tc_single_format_param, test_single_format_param_empty);
+  s = suite_create("ft_printf");
+  tc_single_format_param = tcase_create("Single format param");
+  tcase_add_checked_fixture(tc_single_format_param, setup_ft_printf, teardown_ft_printf);
+  tcase_add_test(tc_single_format_param, test_single_format_param);
+  tcase_add_test(tc_single_format_param, test_single_format_param_null);
+  tcase_add_test(tc_single_format_param, test_single_format_param_empty);
+
+  tc_single_param = tcase_create("Single param");
+  tcase_add_checked_fixture(tc_single_param, setup_ft_printf, teardown_ft_printf);
+  tcase_add_test(tc_single_param, test_single_param_char);
 	
-	suite_add_tcase(s, tc_single_format_param);
+  suite_add_tcase(s, tc_single_format_param);
+  suite_add_tcase(s, tc_single_param);
 
-	return s;
+  return s;
 }
 
 int main(int argc, char *argv[])
 {
-  	int number_failed;
-	Suite *suite;
-	SRunner *srunner;
+  int number_failed;
+  Suite *suite;
+  SRunner *srunner;
 
-	suite = ft_printf_suite();
-	srunner = srunner_create(suite);
-	srunner_run_all(srunner, CK_NORMAL);
-	number_failed = srunner_ntests_failed(srunner);
-	srunner_free(srunner);
+  suite = ft_printf_suite();
+  srunner = srunner_create(suite);
+  srunner_run_all(srunner, CK_NORMAL);
+  number_failed = srunner_ntests_failed(srunner);
+  srunner_free(srunner);
 
-	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
