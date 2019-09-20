5d087358a453a97fb570d16f700e988dd5cc7171
lefrostx <lefrostx@gmail.com>
2019-09-20 19:02:53 +0500

    Added test for hhu single param

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -282,6 +282,17 @@ START_TEST(test_single_param_lli)
 }
 END_TEST
 
+START_TEST(test_single_param_llu)
+{
+  char buffer[128];
+  unsigned long a = (1UL << 63) + (1UL << 16) + (1 << 20);
+  int actual_result = ft_printf("%llu", a);
+  int expected_result = snprintf(buffer, sizeof buffer, "%llu", a);
+  ck_assert_int_eq(actual_result, expected_result);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -325,6 +336,7 @@ Suite *ft_printf_suite(void)
 
   tcase_add_test(tc_single_param, test_single_param_lld);
   tcase_add_test(tc_single_param, test_single_param_lli);
+  tcase_add_test(tc_single_param, test_single_param_llu);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
