1b2d5e4092cf7b574392b449ba5ac316ee6add7b
lefrostx <lefrostx@gmail.com>
2019-09-21 11:22:45 +0500

    Added test for %hho single parameter

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -304,6 +304,17 @@ START_TEST(test_single_param_o)
 }
 END_TEST
 
+START_TEST(test_single_param_hho)
+{
+  char buffer[128];
+  int a = -130;
+  int actual_result = ft_printf("%hho", a);
+  int expected_result = snprintf(buffer, sizeof buffer, "%hho", a);
+  ck_assert_int_eq(actual_result, expected_result);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -350,6 +361,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_single_param, test_single_param_llu);
 
   tcase_add_test(tc_single_param, test_single_param_o);
+  tcase_add_test(tc_single_param, test_single_param_hho);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
