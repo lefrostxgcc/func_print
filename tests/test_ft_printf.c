17a76a5f35e8d03f8b28117139ce310708eff8a4
lefrostx <lefrostx@gmail.com>
2019-09-24 09:54:13 +0500

    Added test for %5p

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -723,6 +723,18 @@ START_TEST(test_width_p_equal)
 }
 END_TEST
 
+START_TEST(test_width_p_smaller)
+{
+  char buffer[128];
+  const char *format = "%5p";
+  unsigned a = 123;
+  int actual_result = ft_printf(format, &a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, &a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -817,6 +829,7 @@ Suite *ft_printf_suite(void)
 
   tcase_add_test(tc_width, test_width_p_greater);
   tcase_add_test(tc_width, test_width_p_equal);
+  tcase_add_test(tc_width, test_width_p_smaller);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
