0c9b4a6c3d29c9d493e27511092303c4df3d6655
lefrostx <lefrostx@gmail.com>
2019-09-24 12:30:42 +0500

    Added test for "[%.o]", 0321

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1035,6 +1035,18 @@ START_TEST(test_precision_o_zero)
 }
 END_TEST
 
+START_TEST(test_precision_o_dot)
+{
+  char buffer[128];
+  const char *format = "[%.o]";
+  unsigned a = 0321;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1164,6 +1176,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_precision_o_greater);
   tcase_add_test(tc_precision, test_precision_o_equal);
   tcase_add_test(tc_precision, test_precision_o_zero);
+  tcase_add_test(tc_precision, test_precision_o_dot);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
