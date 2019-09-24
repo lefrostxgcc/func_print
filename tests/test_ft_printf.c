d2c62cb6ca64a4734a46abdaecd8b66f591aa6eb
lefrostx <lefrostx@gmail.com>
2019-09-24 16:27:44 +0500

    Added test for "[%+10X]", 0xabcdef

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1743,6 +1743,18 @@ START_TEST(test_plus_x_width_smaller)
 }
 END_TEST
 
+START_TEST(test_plus_X_width_greater)
+{
+  char buffer[128];
+  const char *format = "[%+10X]";
+  unsigned a = 0xabcdef;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1954,6 +1966,8 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_plus_x_width_greater);
   tcase_add_test(tc_precision, test_plus_x_width_equal);
   tcase_add_test(tc_precision, test_plus_x_width_smaller);
+
+  tcase_add_test(tc_precision, test_plus_X_width_greater);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
