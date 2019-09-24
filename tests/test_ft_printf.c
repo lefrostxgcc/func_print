7ad3487da32e0c8adab880962c9334675ec417db
lefrostx <lefrostx@gmail.com>
2019-09-24 11:32:14 +0500

    Added test for "[%.4d]", 9876

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -831,6 +831,18 @@ START_TEST(test_precision_d_smaller)
 }
 END_TEST
 
+START_TEST(test_precision_d_equal)
+{
+  char buffer[128];
+  const char *format = "[%.4d]";
+  int a = 9876;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -940,6 +952,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_precision_s_dot);
 
   tcase_add_test(tc_precision, test_precision_d_smaller);
+  tcase_add_test(tc_precision, test_precision_d_equal);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
