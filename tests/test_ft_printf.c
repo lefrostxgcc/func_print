f6a5eb3ba2eede170c952eba2bd628d4d38449cd
lefrostx <lefrostx@gmail.com>
2019-09-24 09:42:59 +0500

    Added test for %16x, 0xf143

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -627,6 +627,18 @@ START_TEST(test_width_o_smaller)
 }
 END_TEST
 
+START_TEST(test_width_x_greater)
+{
+  char buffer[128];
+  const char *format = "%16x";
+  unsigned a = 0xf143;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -710,6 +722,8 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_width, test_width_o_greater);
   tcase_add_test(tc_width, test_width_o_equal);
   tcase_add_test(tc_width, test_width_o_smaller);
+
+  tcase_add_test(tc_width, test_width_x_greater);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
