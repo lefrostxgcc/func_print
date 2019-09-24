cdeefd47d71db35f2ed424fa65904f3e1869fb40
lefrostx <lefrostx@gmail.com>
2019-09-24 16:15:49 +0500

    Added test for "[%+3u]", 123

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1647,6 +1647,18 @@ START_TEST(test_plus_u_width_greater)
 }
 END_TEST
 
+START_TEST(test_plus_u_width_equal)
+{
+  char buffer[128];
+  const char *format = "[%+3u]";
+  unsigned a = 123;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1848,6 +1860,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_plus_i_width_smaller);
 
   tcase_add_test(tc_precision, test_plus_u_width_greater);
+  tcase_add_test(tc_precision, test_plus_u_width_equal);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
