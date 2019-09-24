159f5fa32a15f2fea682d3df4ee5d139ff0a5fda
lefrostx <lefrostx@gmail.com>
2019-09-24 12:33:19 +0500

    Added test for "[%.15x]", 0xa12bc

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1059,6 +1059,18 @@ START_TEST(test_precision_o_smaller)
 }
 END_TEST
 
+START_TEST(test_precision_x_greater)
+{
+  char buffer[128];
+  const char *format = "[%.15x]";
+  unsigned a = 0xa12bc;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1190,6 +1202,8 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_precision_o_zero);
   tcase_add_test(tc_precision, test_precision_o_dot);
   tcase_add_test(tc_precision, test_precision_o_smaller);
+
+  tcase_add_test(tc_precision, test_precision_x_greater);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
