53393c1990c93084dfa84f372837c487668adf0e
lefrostx <lefrostx@gmail.com>
2019-09-24 14:09:18 +0500

    Added test for "[%-2s]", "Text"

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1263,6 +1263,18 @@ START_TEST(test_minus_s_width_greater)
 }
 END_TEST
 
+START_TEST(test_minus_s_width_smaller)
+{
+  char buffer[128];
+  const char *format = "[%-2s]";
+  const char *s = "Text";
+  int actual_result = ft_printf(format, s);
+  int expected_result = snprintf(buffer, sizeof buffer, format, s);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1418,6 +1430,7 @@ Suite *ft_printf_suite(void)
   tcase_add_checked_fixture(tc_minus, setup_ft_printf, teardown_ft_printf);
   tcase_add_test(tc_precision, test_minus_s);
   tcase_add_test(tc_precision, test_minus_s_width_greater);
+  tcase_add_test(tc_precision, test_minus_s_width_smaller);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
