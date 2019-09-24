359e76a6cdd3f8c8dac2d1b8f0e62a27bfdd4169
lefrostx <lefrostx@gmail.com>
2019-09-24 14:42:04 +0500

    Added test for "[%-5p]"

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1503,6 +1503,18 @@ START_TEST(test_minus_X_width_greater)
 }
 END_TEST
 
+START_TEST(test_minus_p_width_smaller)
+{
+  char buffer[128];
+  const char *format = "[%-5p]";
+  unsigned a = 0xabcdef;
+  int actual_result = ft_printf(format, &a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, &a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1684,6 +1696,8 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_minus_X_width_smaller);
   tcase_add_test(tc_precision, test_minus_X_width_equal);
   tcase_add_test(tc_precision, test_minus_X_width_greater);
+
+  tcase_add_test(tc_precision, test_minus_p_width_smaller);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
