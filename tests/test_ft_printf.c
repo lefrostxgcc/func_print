1f659e98e5c0e7c7a874f2e55b8f42d8c87a9110
lefrostx <lefrostx@gmail.com>
2019-09-24 12:02:29 +0500

    Added test for "[%.0i]", -5678

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -903,6 +903,18 @@ START_TEST(test_precision_i_equal)
 }
 END_TEST
 
+START_TEST(test_precision_i_zero)
+{
+  char buffer[128];
+  const char *format = "[%.0i]";
+  int a = -5678;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1019,6 +1031,7 @@ Suite *ft_printf_suite(void)
 
   tcase_add_test(tc_precision, test_precision_i_greater);
   tcase_add_test(tc_precision, test_precision_i_equal);
+  tcase_add_test(tc_precision, test_precision_i_zero);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
