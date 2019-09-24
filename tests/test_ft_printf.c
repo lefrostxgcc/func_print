96a21aa4fb02940956fefd24d57e9c47024b67cc
lefrostx <lefrostx@gmail.com>
2019-09-24 09:50:11 +0500

    Added test for %2X, 0x75Ce4f

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -687,6 +687,18 @@ START_TEST(test_width_X_equal)
 }
 END_TEST
 
+START_TEST(test_width_X_smaller)
+{
+  char buffer[128];
+  const char *format = "%2X";
+  unsigned a = 0x75Ce4f;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -777,6 +789,7 @@ Suite *ft_printf_suite(void)
 
   tcase_add_test(tc_width, test_width_X_greater);
   tcase_add_test(tc_width, test_width_X_equal);
+  tcase_add_test(tc_width, test_width_X_smaller);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
