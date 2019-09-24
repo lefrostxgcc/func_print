c13b93532a8cc6662133373abd7f59f4e191e736
lefrostx <lefrostx@gmail.com>
2019-09-24 14:30:15 +0500

    Added test for "[%-3o]", 0123

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1407,6 +1407,18 @@ START_TEST(test_minus_o_width_smaller)
 }
 END_TEST
 
+START_TEST(test_minus_o_width_equal)
+{
+  char buffer[128];
+  const char *format = "[%-3o]";
+  unsigned a = 0123;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1578,6 +1590,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_minus_u_width_greater);
 
   tcase_add_test(tc_precision, test_minus_o_width_smaller);
+  tcase_add_test(tc_precision, test_minus_o_width_equal);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
