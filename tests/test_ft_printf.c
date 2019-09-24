97fe84633c60ee6d26b79525f7f2d3aa395a7db8
lefrostx <lefrostx@gmail.com>
2019-09-24 14:38:25 +0500

    Added test for "[%-3X]", 0xabcdef

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1467,6 +1467,18 @@ START_TEST(test_minus_x_width_greater)
 }
 END_TEST
 
+START_TEST(test_minus_X_width_smaller)
+{
+  char buffer[128];
+  const char *format = "[%-3X]";
+  unsigned a = 0xabcdef;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1644,6 +1656,8 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_minus_x_width_smaller);
   tcase_add_test(tc_precision, test_minus_x_width_equal);
   tcase_add_test(tc_precision, test_minus_x_width_greater);
+
+  tcase_add_test(tc_precision, test_minus_X_width_smaller);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
