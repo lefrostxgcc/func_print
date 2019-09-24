b43dca0aa1bb2fe9e6da1463ec45769961f5bdcb
lefrostx <lefrostx@gmail.com>
2019-09-24 19:27:07 +0500

    Added test for "[%0X]", 0x1234567

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1911,6 +1911,18 @@ START_TEST(test_zero_x)
 }
 END_TEST
 
+START_TEST(test_zero_X)
+{
+  char buffer[128];
+  const char *format = "[%0X]";
+  unsigned int a = 0x1234567;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -2146,6 +2158,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_pound, test_zero_u);
   tcase_add_test(tc_pound, test_zero_o);
   tcase_add_test(tc_pound, test_zero_x);
+  tcase_add_test(tc_pound, test_zero_X);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
