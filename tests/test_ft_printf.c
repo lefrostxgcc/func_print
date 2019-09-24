95cb9105526f5fd3bc74fd9f03c5f2d3e6199007
lefrostx <lefrostx@gmail.com>
2019-09-24 09:52:00 +0500

    Added test for %20p

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -699,6 +699,18 @@ START_TEST(test_width_X_smaller)
 }
 END_TEST
 
+START_TEST(test_width_p_greater)
+{
+  char buffer[128];
+  const char *format = "%20p";
+  unsigned a = 7;
+  int actual_result = ft_printf(format, &a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, &a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -790,6 +802,8 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_width, test_width_X_greater);
   tcase_add_test(tc_width, test_width_X_equal);
   tcase_add_test(tc_width, test_width_X_smaller);
+
+  tcase_add_test(tc_width, test_width_p_greater);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
