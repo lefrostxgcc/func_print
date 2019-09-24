144596abf41a114c8d5342114a733ecb9a7f15da
lefrostx <lefrostx@gmail.com>
2019-09-24 11:33:56 +0500

    Added test for "[%.0d]", 123456789

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -843,6 +843,18 @@ START_TEST(test_precision_d_equal)
 }
 END_TEST
 
+START_TEST(test_precision_d_zero)
+{
+  char buffer[128];
+  const char *format = "[%.0d]";
+  int a = 123456789;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -953,6 +965,7 @@ Suite *ft_printf_suite(void)
 
   tcase_add_test(tc_precision, test_precision_d_smaller);
   tcase_add_test(tc_precision, test_precision_d_equal);
+  tcase_add_test(tc_precision, test_precision_d_zero);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
