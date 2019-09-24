8bbdad5df706a274cea692f2d984fe79d2ea37f9
lefrostx <lefrostx@gmail.com>
2019-09-24 14:19:44 +0500

    Added test for "[%-5i]", -1234

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1335,6 +1335,18 @@ START_TEST(test_minus_i_width_smaller)
 }
 END_TEST
 
+START_TEST(test_minus_i_width_equal)
+{
+  char buffer[128];
+  const char *format = "[%-5i]";
+  int a = -1234;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1498,6 +1510,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_minus_d_width_greater);
 
   tcase_add_test(tc_precision, test_minus_i_width_smaller);
+  tcase_add_test(tc_precision, test_minus_i_width_equal);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
