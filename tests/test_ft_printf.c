eb5c846619609c641a51ef962f47fc6cbdd3ab7c
lefrostx <lefrostx@gmail.com>
2019-09-24 14:14:53 +0500

    Added test for "[%-10d]" 1234567

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1311,6 +1311,18 @@ START_TEST(test_minus_d_width_equal)
 }
 END_TEST
 
+START_TEST(test_minus_d_width_greater)
+{
+  char buffer[128];
+  const char *format = "[%-10d]";
+  int a = 1234567;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1471,6 +1483,7 @@ Suite *ft_printf_suite(void)
 
   tcase_add_test(tc_precision, test_minus_d_width_smaller);
   tcase_add_test(tc_precision, test_minus_d_width_equal);
+  tcase_add_test(tc_precision, test_minus_d_width_greater);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
