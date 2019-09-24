b7bc5dd91972ca9ad24d4db9ff0500607df24a3c
lefrostx <lefrostx@gmail.com>
2019-09-24 09:53:09 +0500

    Added test for %14p

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -711,6 +711,18 @@ START_TEST(test_width_p_greater)
 }
 END_TEST
 
+START_TEST(test_width_p_equal)
+{
+  char buffer[128];
+  const char *format = "%14p";
+  unsigned a = 10;
+  int actual_result = ft_printf(format, &a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, &a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -804,6 +816,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_width, test_width_X_smaller);
 
   tcase_add_test(tc_width, test_width_p_greater);
+  tcase_add_test(tc_width, test_width_p_equal);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
