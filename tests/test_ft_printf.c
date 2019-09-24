6d26fd95c025ce7f27a10e95f5868ed58227eec3
lefrostx <lefrostx@gmail.com>
2019-09-24 14:47:02 +0500

    Added test for "[%-5c]", '2'

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1551,6 +1551,18 @@ START_TEST(test_minus_c_width_equal)
 }
 END_TEST
 
+START_TEST(test_minus_c_width_greater)
+{
+  char buffer[128];
+  const char *format = "[%-5c]";
+  char a = '2';
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1738,6 +1750,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_minus_p_width_greater);
 
   tcase_add_test(tc_precision, test_minus_c_width_equal);
+  tcase_add_test(tc_precision, test_minus_c_width_greater);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
