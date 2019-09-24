978d717c242addce0cacefab1bc50944bcd51dc4
lefrostx <lefrostx@gmail.com>
2019-09-24 16:02:41 +0500

    Added test for "[%+4i]", -123

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1611,6 +1611,18 @@ START_TEST(test_plus_i_width_greater)
 }
 END_TEST
 
+START_TEST(test_plus_i_width_equal)
+{
+  char buffer[128];
+  const char *format = "[%+4i]";
+  int a = -123;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1808,6 +1820,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_plus_d_width_smaller);
 
   tcase_add_test(tc_precision, test_plus_i_width_greater);
+  tcase_add_test(tc_precision, test_plus_i_width_equal);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
