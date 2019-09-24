ca02ce2d1a47af3d25743722846ee2bb428f83f1
lefrostx <lefrostx@gmail.com>
2019-09-24 12:46:46 +0500

    Added test for "[%.0X]", 0xfb3b

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1143,6 +1143,18 @@ START_TEST(test_precision_X_equal)
 }
 END_TEST
 
+START_TEST(test_precision_X_zero)
+{
+  char buffer[128];
+  const char *format = "[%.0X]";
+  unsigned a = 0xfb3b;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1283,6 +1295,7 @@ Suite *ft_printf_suite(void)
 
   tcase_add_test(tc_precision, test_precision_X_greater);
   tcase_add_test(tc_precision, test_precision_X_equal);
+  tcase_add_test(tc_precision, test_precision_X_zero);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
