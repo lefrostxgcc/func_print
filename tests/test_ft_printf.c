c09b5fe710eb0127acf5055fdf706dfe71f471df
lefrostx <lefrostx@gmail.com>
2019-09-24 11:26:22 +0500

    Added test for "[%.0s]", "123456789"

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -795,6 +795,18 @@ START_TEST(test_precision_s_smaller)
 }
 END_TEST
 
+START_TEST(test_precision_s_zero)
+{
+  char buffer[128];
+  const char *format = "[%.0s]";
+  const char *s = "123456789";
+  int actual_result = ft_printf(format, s);
+  int expected_result = snprintf(buffer, sizeof buffer, format, s);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -900,6 +912,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_precision_s_greater);
   tcase_add_test(tc_precision, test_precision_s_equal);
   tcase_add_test(tc_precision, test_precision_s_smaller);
+  tcase_add_test(tc_precision, test_precision_s_zero);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
