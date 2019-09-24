35ba9cc1531c30e50a3a5b4076f5639f557e3f76
lefrostx <lefrostx@gmail.com>
2019-09-24 12:55:03 +0500

    Added test for "[%.0p]"

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1203,6 +1203,18 @@ START_TEST(test_precision_p_equal)
 }
 END_TEST
 
+START_TEST(test_precision_p_zero)
+{
+  char buffer[128];
+  const char *format = "[%.0p]";
+  unsigned a = 1;
+  int actual_result = ft_printf(format, &a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, &a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1349,6 +1361,7 @@ Suite *ft_printf_suite(void)
 
   tcase_add_test(tc_precision, test_precision_p_smaller);
   tcase_add_test(tc_precision, test_precision_p_equal);
+  tcase_add_test(tc_precision, test_precision_p_zero);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
