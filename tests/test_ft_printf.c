4e1c81d5ce643901df967ba05f4c4596fc89aff3
lefrostx <lefrostx@gmail.com>
2019-09-24 12:39:34 +0500

    Added test for "[%.4x]", 0xffff15

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1107,6 +1107,18 @@ START_TEST(test_precision_x_dot)
 }
 END_TEST
 
+START_TEST(test_precision_x_smaller)
+{
+  char buffer[128];
+  const char *format = "[%.4x]";
+  unsigned a = 0xffff15;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1243,6 +1255,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_precision_x_equal);
   tcase_add_test(tc_precision, test_precision_x_zero);
   tcase_add_test(tc_precision, test_precision_x_dot);
+  tcase_add_test(tc_precision, test_precision_x_smaller);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
