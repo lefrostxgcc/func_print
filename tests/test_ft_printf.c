8b38061ebed2fe6958400103660b7c4f53527349
lefrostx <lefrostx@gmail.com>
2019-09-24 12:45:24 +0500

    Added test for "[%.2X]", 0xbc

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1131,6 +1131,18 @@ START_TEST(test_precision_X_greater)
 }
 END_TEST
 
+START_TEST(test_precision_X_equal)
+{
+  char buffer[128];
+  const char *format = "[%.2X]";
+  unsigned a = 0xbc;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1270,6 +1282,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_precision_x_smaller);
 
   tcase_add_test(tc_precision, test_precision_X_greater);
+  tcase_add_test(tc_precision, test_precision_X_equal);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
