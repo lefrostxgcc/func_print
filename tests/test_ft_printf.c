93f1f17cf5360ea61fae110eb869c042128ac8bb
lefrostx <lefrostx@gmail.com>
2019-09-24 12:01:16 +0500

    Added test for "[%.5i]", -98765

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -891,6 +891,18 @@ START_TEST(test_precision_i_greater)
 }
 END_TEST
 
+START_TEST(test_precision_i_equal)
+{
+  char buffer[128];
+  const char *format = "[%.5i]";
+  int a = -98765;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1006,6 +1018,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_precision_d_greater);
 
   tcase_add_test(tc_precision, test_precision_i_greater);
+  tcase_add_test(tc_precision, test_precision_i_equal);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
