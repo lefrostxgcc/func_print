c34f73cfbb5339e3c18caf7bb51e7acc9632c8ba
lefrostx <lefrostx@gmail.com>
2019-09-24 14:39:35 +0500

    Added test for "[%-6X]", 0xabcdef

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1479,6 +1479,18 @@ START_TEST(test_minus_X_width_smaller)
 }
 END_TEST
 
+START_TEST(test_minus_X_width_equal)
+{
+  char buffer[128];
+  const char *format = "[%-6X]";
+  unsigned a = 0xabcdef;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1658,6 +1670,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_minus_x_width_greater);
 
   tcase_add_test(tc_precision, test_minus_X_width_smaller);
+  tcase_add_test(tc_precision, test_minus_X_width_equal);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
