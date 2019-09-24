2d04cc9f22129998c2250ab0368c060f821edd89
lefrostx <lefrostx@gmail.com>
2019-09-24 12:24:16 +0500

    Added test for "[%.u]", 1234

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -975,6 +975,18 @@ START_TEST(test_precision_u_zero)
 }
 END_TEST
 
+START_TEST(test_precision_u_dot)
+{
+  char buffer[128];
+  const char *format = "[%.u]";
+  unsigned a = 1234;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1098,6 +1110,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_precision_u_greater);
   tcase_add_test(tc_precision, test_precision_u_equal);
   tcase_add_test(tc_precision, test_precision_u_zero);
+  tcase_add_test(tc_precision, test_precision_u_dot);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
