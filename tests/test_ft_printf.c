439c230fda7aecc825c6bf9f5d1ca8a07de7923d
lefrostx <lefrostx@gmail.com>
2019-09-24 11:30:29 +0500

    Added test for "[%.3d]", 123456

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -819,6 +819,18 @@ START_TEST(test_precision_s_dot)
 }
 END_TEST
 
+START_TEST(test_precision_d_smaller)
+{
+  char buffer[128];
+  const char *format = "[%.3d]";
+  int a = 123456;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -926,6 +938,8 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_precision_s_smaller);
   tcase_add_test(tc_precision, test_precision_s_zero);
   tcase_add_test(tc_precision, test_precision_s_dot);
+
+  tcase_add_test(tc_precision, test_precision_d_smaller);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
