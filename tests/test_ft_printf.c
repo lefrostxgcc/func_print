89d7bb1028cd598f6b684fb995661da81e3a7b6b
lefrostx <lefrostx@gmail.com>
2019-09-24 09:35:49 +0500

    Added test for %6u, 555444

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -567,6 +567,18 @@ START_TEST(test_width_u_greater)
 }
 END_TEST
 
+START_TEST(test_width_u_equal)
+{
+  char buffer[128];
+  const char *format = "%6u";
+  unsigned a = 555444;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -644,6 +656,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_width, test_width_d_smaller);
 
   tcase_add_test(tc_width, test_width_u_greater);
+  tcase_add_test(tc_width, test_width_u_equal);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
