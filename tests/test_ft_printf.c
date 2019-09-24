0645a604cc3a69ecdeab630591961196d87c6527
lefrostx <lefrostx@gmail.com>
2019-09-24 14:28:30 +0500

    Added test for "[%-2o]", 01234

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1395,6 +1395,18 @@ START_TEST(test_minus_u_width_greater)
 }
 END_TEST
 
+START_TEST(test_minus_o_width_smaller)
+{
+  char buffer[128];
+  const char *format = "[%-2o]";
+  unsigned a = 01234;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1564,6 +1576,8 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_minus_u_width_smaller);
   tcase_add_test(tc_precision, test_minus_u_width_equal);
   tcase_add_test(tc_precision, test_minus_u_width_greater);
+
+  tcase_add_test(tc_precision, test_minus_o_width_smaller);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
