d2a4bb8c0b89d254345ab9fe2ccfaf121f018580
lefrostx <lefrostx@gmail.com>
2019-09-24 16:18:37 +0500

    Added test for "[%+8o]", 01234

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1671,6 +1671,18 @@ START_TEST(test_plus_u_width_smaller)
 }
 END_TEST
 
+START_TEST(test_plus_o_width_greater)
+{
+  char buffer[128];
+  const char *format = "[%+8o]";
+  unsigned a = 01234;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1874,6 +1886,8 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_plus_u_width_greater);
   tcase_add_test(tc_precision, test_plus_u_width_equal);
   tcase_add_test(tc_precision, test_plus_u_width_smaller);
+
+  tcase_add_test(tc_precision, test_plus_o_width_greater);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
