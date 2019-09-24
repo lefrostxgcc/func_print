f8e0bdd95ea451545ee10a659977c9bfd2aeeb9c
lefrostx <lefrostx@gmail.com>
2019-09-24 16:20:15 +0500

    Added test for "[%+4o]", 01234

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1683,6 +1683,18 @@ START_TEST(test_plus_o_width_greater)
 }
 END_TEST
 
+START_TEST(test_plus_o_width_equal)
+{
+  char buffer[128];
+  const char *format = "[%+4o]";
+  unsigned a = 01234;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1888,6 +1900,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_plus_u_width_smaller);
 
   tcase_add_test(tc_precision, test_plus_o_width_greater);
+  tcase_add_test(tc_precision, test_plus_o_width_equal);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
