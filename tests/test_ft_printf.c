bbe452a1a77675c1d96a7f84d551cc06ca2d8dc1
lefrostx <lefrostx@gmail.com>
2019-09-24 12:54:09 +0500

    Added test for "[%.12p]"

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1191,6 +1191,18 @@ START_TEST(test_precision_p_smaller)
 }
 END_TEST
 
+START_TEST(test_precision_p_equal)
+{
+  char buffer[128];
+  const char *format = "[%.12p]";
+  unsigned a = 1;
+  int actual_result = ft_printf(format, &a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, &a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1336,6 +1348,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_precision_X_smaller);
 
   tcase_add_test(tc_precision, test_precision_p_smaller);
+  tcase_add_test(tc_precision, test_precision_p_equal);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
