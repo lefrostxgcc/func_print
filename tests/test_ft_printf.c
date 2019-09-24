ecbceb03d5cb96a4ac0d25fac41f84ca204fc87d
lefrostx <lefrostx@gmail.com>
2019-09-24 12:37:25 +0500

    Added test for "[%.0x]", 0x123fe

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1083,6 +1083,18 @@ START_TEST(test_precision_x_equal)
 }
 END_TEST
 
+START_TEST(test_precision_x_zero)
+{
+  char buffer[128];
+  const char *format = "[%.0x]";
+  unsigned a = 0x123fe;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -1217,6 +1229,7 @@ Suite *ft_printf_suite(void)
 
   tcase_add_test(tc_precision, test_precision_x_greater);
   tcase_add_test(tc_precision, test_precision_x_equal);
+  tcase_add_test(tc_precision, test_precision_x_zero);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
