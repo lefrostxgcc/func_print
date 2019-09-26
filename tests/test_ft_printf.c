93146f77402f3dbc1b2d649f6bd94fcc043025d8
lefrostx <lefrostx@gmail.com>
2019-09-26 13:11:08 +0500

    Added test for "[%.3f]", -1.2345

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -2043,6 +2043,18 @@ START_TEST(test_float_small_negative)
 }
 END_TEST
 
+START_TEST(test_float_small_precision)
+{
+  char buffer[128];
+  const char *format = "[%.3f]";
+  double a = -1.2345;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -2294,6 +2306,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_float, test_float_small_negative_int);
   tcase_add_test(tc_float, test_float_small);
   tcase_add_test(tc_float, test_float_small_negative);
+  tcase_add_test(tc_float, test_float_small_precision);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
