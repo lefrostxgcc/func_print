6ec5465ad586ee81486149a2b2180daba5180091
lefrostx <lefrostx@gmail.com>
2019-09-26 13:40:18 +0500

    Added test for "[%12f]", -1.23456

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -2079,6 +2079,18 @@ START_TEST(test_float_width_greater)
 }
 END_TEST
 
+START_TEST(test_float_width_greater_negative)
+{
+  char buffer[128];
+  const char *format = "[%12f]";
+  double a = -1.23456;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -2333,6 +2345,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_float, test_float_small_precision);
   tcase_add_test(tc_float, test_float_big_precision);
   tcase_add_test(tc_float, test_float_width_greater);
+  tcase_add_test(tc_float, test_float_width_greater_negative);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
