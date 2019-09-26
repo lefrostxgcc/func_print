20b0e7b683d490db9ae95b9be5ad78c5c27cdea8
lefrostx <lefrostx@gmail.com>
2019-09-26 13:08:23 +0500

    Added test for "[%f]", -12.34

--- rz_ftoa.c
+++ rz_ftoa.c
@@ -6,6 +6,7 @@ void rz_ftoa(struct arg_info *info, double arg, long *intp, unsigned long *fracp
 {
   int precision;
   int i;
+  int v;
 
   *intp = (long) arg;
   arg -= *intp;
@@ -13,11 +14,16 @@ void rz_ftoa(struct arg_info *info, double arg, long *intp, unsigned long *fracp
   if (precision < 0)
     precision = 6;
   i = 0;
+  if (arg < 0)
+    arg = -arg;
+  v = 1;
   while (i < precision)
     {
-      arg *= 10;
+      v *= 10;
       i++;
     }
+  arg *= v;
+  arg += 1.0 / v;
   info->precision = -1;
   *fracp = arg;
 }
--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -2031,6 +2031,18 @@ START_TEST(test_float_small)
 }
 END_TEST
 
+START_TEST(test_float_small_negative)
+{
+  char buffer[128];
+  const char *format = "[%f]";
+  double a = -12.34;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -2281,6 +2293,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_float, test_float_small_int);
   tcase_add_test(tc_float, test_float_small_negative_int);
   tcase_add_test(tc_float, test_float_small);
+  tcase_add_test(tc_float, test_float_small_negative);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
