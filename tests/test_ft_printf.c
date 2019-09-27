5d972cab6e68ba67d44f420ad51f81fd047a624a
lefrostx <lefrostx@gmail.com>
2019-09-27 11:24:22 +0500

    Added test for "[% o]", 01234

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -2187,6 +2187,18 @@ START_TEST(test_space_u)
 }
 END_TEST
 
+START_TEST(test_space_o)
+{
+  char buffer[128];
+  const char *format = "[% o]";
+  unsigned int a = 01234;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -2454,6 +2466,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_space, test_space_d);
   tcase_add_test(tc_space, test_space_i_negative);
   tcase_add_test(tc_space, test_space_u);
+  tcase_add_test(tc_space, test_space_o);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
