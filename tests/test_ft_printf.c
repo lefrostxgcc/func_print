7b65971a7130bf170b4f97afd17b2f5ae78496fc
lefrostx <lefrostx@gmail.com>
2019-09-27 11:25:25 +0500

    Added test for "[% x]", 0xabcdef12

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -2199,6 +2199,18 @@ START_TEST(test_space_o)
 }
 END_TEST
 
+START_TEST(test_space_x)
+{
+  char buffer[128];
+  const char *format = "[% x]";
+  unsigned int a = 0xabcdef12;
+  int actual_result = ft_printf(format, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -2467,6 +2479,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_space, test_space_i_negative);
   tcase_add_test(tc_space, test_space_u);
   tcase_add_test(tc_space, test_space_o);
+  tcase_add_test(tc_space, test_space_x);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
