314d489a1bbf80c378117638a1876138e968d5bd
lefrostx <lefrostx@gmail.com>
2019-09-28 18:59:33 +0500

    Added test for "[%123]"

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -2353,6 +2353,17 @@ START_TEST(test_omit_type)
 }
 END_TEST
 
+START_TEST(test_width_omit_type)
+{
+  char buffer[128];
+  const char *format = "[%123]";
+  int actual_result = ft_printf(format);
+  int expected_result = snprintf(buffer, sizeof buffer, format);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -2638,6 +2649,7 @@ Suite *ft_printf_suite(void)
   tcase_add_checked_fixture(tc_error_type, setup_ft_printf, teardown_ft_printf);
   tcase_add_test(tc_error_type, test_single_percent);
   tcase_add_test(tc_error_type, test_omit_type);
+  tcase_add_test(tc_error_type, test_width_omit_type);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
