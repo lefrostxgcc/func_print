993955a39952d4a4af5c42c8d11bcf5f9174707a
lefrostx <lefrostx@gmail.com>
2019-09-30 13:07:22 +0500

    Added test for max ushort

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -2801,6 +2801,18 @@ START_TEST(test_limits_min_short)
 }
 END_TEST
 
+START_TEST(test_limits_max_ushort)
+{
+  char buffer[256];
+  const char *format = "%hu %ho %hx %hX";
+  long a = 65535;
+  int actual_result = ft_printf(format, a, a, a, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a, a, a, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -3129,6 +3141,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_limits, test_limits_one_below_lu);
   tcase_add_test(tc_limits, test_limits_max_short);
   tcase_add_test(tc_limits, test_limits_min_short);
+  tcase_add_test(tc_limits, test_limits_max_ushort);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
