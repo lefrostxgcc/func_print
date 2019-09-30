35fce7f148ecbee3d6ff16087ca4fe51357cfb1f
lefrostx <lefrostx@gmail.com>
2019-09-30 12:39:08 +0500

    Added test for one above %hh

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -2633,6 +2633,18 @@ START_TEST(test_limits_one_below_hhd)
 }
 END_TEST
 
+START_TEST(test_limits_one_above_hhu)
+{
+  char buffer[256];
+  const char *format = "%hhu %hho %hhx %hhX";
+  long a = 128;
+  int actual_result = ft_printf(format, a, a, a, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a, a, a, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -2947,6 +2959,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_limits, test_limits_min_ulong);
   tcase_add_test(tc_limits, test_limits_one_above_hhd);
   tcase_add_test(tc_limits, test_limits_one_below_hhd);
+  tcase_add_test(tc_limits, test_limits_one_above_hhu);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
