7606d79bc3e93842361ad131015f6518920ec7f0
lefrostx <lefrostx@gmail.com>
2019-09-30 11:06:49 +0500

    Added test for max long

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -2549,6 +2549,18 @@ START_TEST(test_limits_min_uint)
 }
 END_TEST
 
+START_TEST(test_limits_max_long)
+{
+  char buffer[128];
+  const char *format = "%ld %li %lld %lli";
+  long a = 9223372036854775807;
+  int actual_result = ft_printf(format, a, a, a, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a, a, a, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -2856,6 +2868,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_limits, test_limits_zero_int);
   tcase_add_test(tc_limits, test_limits_max_uint);
   tcase_add_test(tc_limits, test_limits_min_uint);
+  tcase_add_test(tc_limits, test_limits_max_long);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
