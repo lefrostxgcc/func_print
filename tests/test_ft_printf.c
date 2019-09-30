93cec4976dad88d73d9b5a8de571d05c9b48235d
lefrostx <lefrostx@gmail.com>
2019-09-30 12:45:55 +0500

    Added test for one above max hu

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -2681,6 +2681,18 @@ START_TEST(test_limits_one_below_hd)
 }
 END_TEST
 
+START_TEST(test_limits_one_above_hu)
+{
+  char buffer[256];
+  const char *format = "%hu %ho %hx %hX";
+  long a = 65536;
+  int actual_result = ft_printf(format, a, a, a, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a, a, a, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -2999,6 +3011,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_limits, test_limits_one_below_hhu);
   tcase_add_test(tc_limits, test_limits_one_above_hd);
   tcase_add_test(tc_limits, test_limits_one_below_hd);
+  tcase_add_test(tc_limits, test_limits_one_above_hu);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
