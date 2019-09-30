9c858771017192304a9ff5e7e392010d34f804f1
lefrostx <lefrostx@gmail.com>
2019-09-30 16:33:26 +0500

    Added test for space minus equal width

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -2472,13 +2472,33 @@ START_TEST(test_space_equal_width)
   const char *format = "% 5.5d % 3.3i % 4.4u % 5.5o % 3.3x % 3.3X"
       "% 7.4f % 7.4lf % 2.4c % 5.5s";
   int a = 12;
+  int a2 = -1;
   double b = 1.46;
   char c = 'q';
   const char *s = "Hello World!";
   int actual_result =
-      ft_printf(format, a, a, a, a, a, a, b, b, c, s);
+      ft_printf(format, a, a2, a, a, a, a, b, b, c, s);
   int expected_result =
-      snprintf(buffer, sizeof buffer, format, a, a, a, a, a, a, b, b, c, s);
+      snprintf(buffer, sizeof buffer, format, a, a2, a, a, a, a, b, b, c, s);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
+START_TEST(test_space_minus_equal_width)
+{
+  char buffer[512];
+  const char *format = "%- 5.5d %- 3.3i % -4.4u %- 5.5o %-- 3.3x %- 3.3X"
+      "%- 7.4f % -7.4lf %- 2.4c % -5.5s";
+  int a = 12;
+  int a2 = -1;
+  double b = 1.46;
+  char c = 'q';
+  const char *s = "Hello World!";
+  int actual_result =
+      ft_printf(format, a, a2, a, a, a, a, b, b, c, s);
+  int expected_result =
+      snprintf(buffer, sizeof buffer, format, a, a2, a, a, a, a, b, b, c, s);
   ck_assert_pstr_eq(get_write_buf(), buffer);
   ck_assert_int_eq(actual_result, expected_result);
 }
@@ -3282,6 +3302,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_space, test_space_lf);
   tcase_add_test(tc_space, test_space_Lf);
   tcase_add_test(tc_space, test_space_equal_width);
+  tcase_add_test(tc_space, test_space_minus_equal_width);
 
   tc_error_type = tcase_create("Error type");
   tcase_add_checked_fixture(tc_error_type, setup_ft_printf, teardown_ft_printf);
