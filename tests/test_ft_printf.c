781622783a5268f44700f334a504aee83bed3a27
lefrostx <lefrostx@gmail.com>
2019-09-30 10:55:16 +0500

    Added test for min uint

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -2537,6 +2537,18 @@ START_TEST(test_limits_max_uint)
 }
 END_TEST
 
+START_TEST(test_limits_min_uint)
+{
+  char buffer[128];
+  const char *format = "%u %o %x %X";
+  unsigned a = 0;
+  int actual_result = ft_printf(format, a, a, a, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a, a, a, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -2843,6 +2855,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_limits, test_limits_min_int);
   tcase_add_test(tc_limits, test_limits_zero_int);
   tcase_add_test(tc_limits, test_limits_max_uint);
+  tcase_add_test(tc_limits, test_limits_min_uint);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
