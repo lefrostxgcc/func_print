d0fdbd9a6bdc59b1f9f16dc5132eeac2d0bfa8bd
lefrostx <lefrostx@gmail.com>
2019-09-30 13:05:23 +0500

    Added test for min short

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -2789,6 +2789,18 @@ START_TEST(test_limits_max_short)
 }
 END_TEST
 
+START_TEST(test_limits_min_short)
+{
+  char buffer[256];
+  const char *format = "%hd %hi";
+  long a = -32768;
+  int actual_result = ft_printf(format, a, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -3116,6 +3128,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_limits, test_limits_one_above_ld);
   tcase_add_test(tc_limits, test_limits_one_below_lu);
   tcase_add_test(tc_limits, test_limits_max_short);
+  tcase_add_test(tc_limits, test_limits_min_short);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
