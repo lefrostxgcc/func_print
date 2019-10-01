7cd9aba975f58642f3311622c1f968912074edad
lefrostx <lefrostx@gmail.com>
2019-10-01 12:46:15 +0500

    Added test for %u zero arg zero precision

--- rz_print_unsigned.c
+++ rz_print_unsigned.c
@@ -30,7 +30,8 @@ void rz_print_type_u(t_rz_buf *buf, t_rz_arg *f, const char *s)
 	rz_buf_fill(buf, ch, padding);
     if (f->precision > f->slen)
 	rz_buf_fill(buf, '0', f->precision - f->slen);
-    rz_buf_add(buf, s, f->slen);
+    if (!(f->precision == 0 && *s == '0'))
+	rz_buf_add(buf, s, f->slen);
     if (f->minus && padding > 0)
 	rz_buf_fill(buf, ' ', padding);
 }
--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1022,6 +1022,18 @@ START_TEST(test_precision_u_smaller)
 }
 END_TEST
 
+START_TEST(test_precision_u_zero_arg)
+{
+  char buffer[128];
+  const char *format = "[%.u|%.0u]";
+  unsigned a = 0;
+  int actual_result = ft_printf(format, a, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 START_TEST(test_precision_o_greater)
 {
   char buffer[128];
@@ -3169,6 +3181,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_precision_u_zero);
   tcase_add_test(tc_precision, test_precision_u_dot);
   tcase_add_test(tc_precision, test_precision_u_smaller);
+  tcase_add_test(tc_precision, test_precision_u_zero_arg);
 
   tcase_add_test(tc_precision, test_precision_o_greater);
   tcase_add_test(tc_precision, test_precision_o_equal);
