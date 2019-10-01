c322c65123c9cb9ec8d0bff05436cac2a63e805f
lefrostx <lefrostx@gmail.com>
2019-10-01 12:56:44 +0500

    Added test for %u sharp zero arg zero precision

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -1034,6 +1034,18 @@ START_TEST(test_precision_u_zero_arg)
 }
 END_TEST
 
+START_TEST(test_precision_u_sharp_zero_arg)
+{
+  char buffer[128];
+  const char *format = "[%#.u|%#.0u]";
+  unsigned a = 0;
+  int actual_result = ft_printf(format, a, a);
+  int expected_result = snprintf(buffer, sizeof buffer, format, a, a);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 START_TEST(test_precision_o_greater)
 {
   char buffer[128];
@@ -3206,6 +3218,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_precision, test_precision_u_dot);
   tcase_add_test(tc_precision, test_precision_u_smaller);
   tcase_add_test(tc_precision, test_precision_u_zero_arg);
+  tcase_add_test(tc_precision, test_precision_u_sharp_zero_arg);
 
   tcase_add_test(tc_precision, test_precision_o_greater);
   tcase_add_test(tc_precision, test_precision_o_equal);
