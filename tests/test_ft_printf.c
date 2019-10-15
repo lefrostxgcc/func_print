800a5e0c13627e38fc8af47207d63b755839bbae
Chip <lefrostx@gmail.com>
2019-10-15 23:35:36 +0500

    Added test for "%2.s", ""

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -3498,6 +3498,17 @@ START_TEST(test_nullchar_width1_pre0)
 }
 END_TEST
 
+START_TEST(test_nullchar_width2_pre0)
+{
+  char buffer[256];
+  const char *format = "%2.s";
+  int ac = ft_printf(format, "");
+  int ex = snprintf(buffer, sizeof buffer, format, "");
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(ac, ex);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -3898,6 +3909,7 @@ Suite *ft_printf_suite(void)
   tc_nullchar = tcase_create("nullchar");
   tcase_add_checked_fixture(tc_nullchar, setup_ft_printf, teardown_ft_printf);
   tcase_add_test(tc_nullchar, test_nullchar_width1_pre0);
+  tcase_add_test(tc_nullchar, test_nullchar_width2_pre0);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
