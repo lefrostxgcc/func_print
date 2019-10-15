a61fa16a3334fb2ad94c67c1575e41a4d78e55a9
Chip <lefrostx@gmail.com>
2019-10-15 19:48:44 +0500

    Added tst for "%10.s", NULL

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -3278,6 +3278,17 @@ START_TEST(test_nulls_width3_pre0)
 }
 END_TEST
 
+START_TEST(test_nulls_width10_pre0)
+{
+  char buffer[256];
+  const char *format = "%10.s";
+  int ac = ft_printf(format, NULL);
+  int ex = snprintf(buffer, sizeof buffer, format, NULL);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(ac, ex);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -3654,6 +3665,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_nulls, test_nulls_width_minus3);
   tcase_add_test(tc_nulls, test_nulls_width_09);
   tcase_add_test(tc_nulls, test_nulls_width3_pre0);
+  tcase_add_test(tc_nulls, test_nulls_width10_pre0);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
