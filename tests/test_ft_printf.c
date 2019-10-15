b85ee63facb71c822f12da71128d543bfced10f3
Chip <lefrostx@gmail.com>
2019-10-15 20:31:55 +0500

    Added test for "%6.6s", NULL

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -3454,6 +3454,17 @@ START_TEST(test_nulls_width20_pre6)
 }
 END_TEST
 
+START_TEST(test_nulls_width6_pre6)
+{
+  char buffer[256];
+  const char *format = "%6.6s";
+  int ac = ft_printf(format, NULL);
+  int ex = snprintf(buffer, sizeof buffer, format, NULL);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(ac, ex);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -3846,6 +3857,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_nulls, test_nulls_width_minus6_pre1);
   tcase_add_test(tc_nulls, test_nulls_width3_pre6);
   tcase_add_test(tc_nulls, test_nulls_width20_pre6);
+  tcase_add_test(tc_nulls, test_nulls_width6_pre6);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
