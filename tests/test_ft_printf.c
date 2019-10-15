fdc9b8ef0362fc97ea1357ca4073fb19d313e975
Chip <lefrostx@gmail.com>
2019-10-16 00:32:54 +0500

    Added test for "%5.u", 0

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -3696,6 +3696,17 @@ START_TEST(test_nullu_width5_pre0)
 }
 END_TEST
 
+START_TEST(test_nullu_width5_predot)
+{
+  char buffer[64];
+  const char *format = "%5.u";
+  int ac = ft_printf(format, 0);
+  int ex = snprintf(buffer, sizeof buffer, format, 0);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(ac, ex);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -4122,6 +4133,7 @@ Suite *ft_printf_suite(void)
   tc_nullu = tcase_create("nullu");
   tcase_add_checked_fixture(tc_nullu, setup_ft_printf, teardown_ft_printf);
   tcase_add_test(tc_nullu, test_nullu_width5_pre0);
+  tcase_add_test(tc_nullu, test_nullu_width5_predot);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
