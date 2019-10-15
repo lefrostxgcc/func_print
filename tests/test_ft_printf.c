b4f175dec840564960e7d8e1d06376489988586f
Chip <lefrostx@gmail.com>
2019-10-15 19:47:18 +0500

    Added test for "%3.s", NULL

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -3267,6 +3267,17 @@ START_TEST(test_nulls_width_09)
 }
 END_TEST
 
+START_TEST(test_nulls_width3_pre0)
+{
+  char buffer[256];
+  const char *format = "%3.s";
+  int ac = ft_printf(format, NULL);
+  int ex = snprintf(buffer, sizeof buffer, format, NULL);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(ac, ex);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -3642,6 +3653,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_nulls, test_nulls_width_minus16);
   tcase_add_test(tc_nulls, test_nulls_width_minus3);
   tcase_add_test(tc_nulls, test_nulls_width_09);
+  tcase_add_test(tc_nulls, test_nulls_width3_pre0);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
