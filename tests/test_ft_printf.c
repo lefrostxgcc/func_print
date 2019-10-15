123bd880630ea321636b3cc9c44ef29308c18968
Chip <lefrostx@gmail.com>
2019-10-15 23:54:00 +0500

    Added test for "%-6.1s", ""

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -3564,6 +3564,17 @@ START_TEST(test_nullchar_width_minus3_pre1)
 }
 END_TEST
 
+START_TEST(test_nullchar_width_minus6_pre1)
+{
+  char buffer[256];
+  const char *format = "%-6.1s";
+  int ac = ft_printf(format, "");
+  int ex = snprintf(buffer, sizeof buffer, format, "");
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(ac, ex);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -3970,6 +3981,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_nullchar, test_nullchar_width_minus2_pre0);
   tcase_add_test(tc_nullchar, test_nullchar_width_minus6_pre0);
   tcase_add_test(tc_nullchar, test_nullchar_width_minus3_pre1);
+  tcase_add_test(tc_nullchar, test_nullchar_width_minus6_pre1);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
