38ce662d44225b69e543b0338dfe4c050f13b6d7
Chip <lefrostx@gmail.com>
2019-10-15 19:40:04 +0500

    Added test for "%2s", NULL

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -3212,6 +3212,17 @@ START_TEST(test_nulls_width32)
 }
 END_TEST
 
+START_TEST(test_nulls_width2)
+{
+  char buffer[256];
+  const char *format = "%2s";
+  int ac = ft_printf(format, NULL);
+  int ex = snprintf(buffer, sizeof buffer, format, NULL);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(ac, ex);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -3582,6 +3593,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_nulls, test_nulls_single);
   tcase_add_test(tc_nulls, test_nulls_fmt_single);
   tcase_add_test(tc_nulls, test_nulls_width32);
+  tcase_add_test(tc_nulls, test_nulls_width2);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
