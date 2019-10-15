8f1206c4696860d37b3d30f8e84b8c0109596b9a
Chip <lefrostx@gmail.com>
2019-10-15 19:52:21 +0500

    Added test for "%9.s", NULL

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -3311,6 +3311,17 @@ START_TEST(test_nulls_width2_pre0)
 }
 END_TEST
 
+START_TEST(test_nulls_width9_pre0)
+{
+  char buffer[256];
+  const char *format = "%9.s";
+  int ac = ft_printf(format, NULL);
+  int ex = snprintf(buffer, sizeof buffer, format, NULL);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(ac, ex);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -3690,6 +3701,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_nulls, test_nulls_width10_pre0);
   tcase_add_test(tc_nulls, test_nulls_width1_pre0);
   tcase_add_test(tc_nulls, test_nulls_width2_pre0);
+  tcase_add_test(tc_nulls, test_nulls_width9_pre0);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
