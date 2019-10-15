c4b59e263433406acbdf1fc76634831d8a3049ba
Chip <lefrostx@gmail.com>
2019-10-16 00:20:36 +0500

    Added test for "%+5.0i", 0

--- rz_print_dif.c
+++ rz_print_dif.c
@@ -20,6 +20,8 @@ void rz_print_type_di(t_rz_buf *buf, t_rz_arg *f, const char *s)
     width = rz_tern_l(f->precision > f->slen, f->precision, f->slen);
     if (f->negative || f->plus || f->space)
 	width++;
+    if (f->argzero && f->precision == 0 && f->width > 0)
+	width--;
     padding = f->width - width;
     if (ch == '0')
 	rz_print_sign_placeholder(buf, f);
@@ -31,8 +33,6 @@ void rz_print_type_di(t_rz_buf *buf, t_rz_arg *f, const char *s)
 	rz_buf_fill(buf, '0', f->precision - f->slen);
     if (!(f->precision == 0 && f->argzero))
 	rz_buf_add(buf, s, f->slen);
-    else if (f->width > 0)
-	rz_buf_fill(buf, ' ', 1);
     if (f->minus && padding > 0)
 	rz_buf_fill(buf, ' ', padding);
 }
--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -3641,6 +3641,17 @@ START_TEST(test_nullint_width_minus5_predot)
 }
 END_TEST
 
+START_TEST(test_nullint_width5_plus_pre0)
+{
+  char buffer[64];
+  const char *format = "%+5.0i";
+  int ac = ft_printf(format, 0);
+  int ex = snprintf(buffer, sizeof buffer, format, 0);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(ac, ex);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -4058,6 +4069,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_nullint, test_nullint_width5_predot);
   tcase_add_test(tc_nullint, test_nullint_width_minus5_pre0);
   tcase_add_test(tc_nullint, test_nullint_width_minus5_predot);
+  tcase_add_test(tc_nullint, test_nullint_width5_plus_pre0);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
