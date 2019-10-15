026877771e6e467be9846caa94d9d4d5baf4c84c
Chip <lefrostx@gmail.com>
2019-10-15 19:43:46 +0500

    Added test for "%-3s", NULL

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -3245,6 +3245,17 @@ START_TEST(test_nulls_width_minus16)
 }
 END_TEST
 
+START_TEST(test_nulls_width_minus3)
+{
+  char buffer[256];
+  const char *format = "%-3s";
+  int ac = ft_printf(format, NULL);
+  int ex = snprintf(buffer, sizeof buffer, format, NULL);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(ac, ex);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -3618,6 +3629,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_nulls, test_nulls_width2);
   tcase_add_test(tc_nulls, test_nulls_width_minus32);
   tcase_add_test(tc_nulls, test_nulls_width_minus16);
+  tcase_add_test(tc_nulls, test_nulls_width_minus3);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
