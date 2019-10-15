a472c7f6ce6f656da2f80337ee893efa77c79af5
Chip <lefrostx@gmail.com>
2019-10-15 19:38:56 +0500

    Added test for "%32s", NULL

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -3201,6 +3201,17 @@ START_TEST(test_nulls_fmt_single)
 }
 END_TEST
 
+START_TEST(test_nulls_width32)
+{
+  char buffer[256];
+  const char *format = "%32s";
+  int ac = ft_printf(format, NULL);
+  int ex = snprintf(buffer, sizeof buffer, format, NULL);
+  ck_assert_pstr_eq(get_write_buf(), buffer);
+  ck_assert_int_eq(ac, ex);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -3570,6 +3581,7 @@ Suite *ft_printf_suite(void)
   tcase_add_checked_fixture(tc_nulls, setup_ft_printf, teardown_ft_printf);
   tcase_add_test(tc_nulls, test_nulls_single);
   tcase_add_test(tc_nulls, test_nulls_fmt_single);
+  tcase_add_test(tc_nulls, test_nulls_width32);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
