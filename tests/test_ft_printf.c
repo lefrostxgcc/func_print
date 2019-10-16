eaefc5da7e0de5bfe4242a7b6b0fd4ed16a14c38
Chip <lefrostx@gmail.com>
2019-10-16 17:47:28 +0500

    Added test for "hello % % %"

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -3926,6 +3926,17 @@ START_TEST(test_prefix_double_percent)
 }
 END_TEST
 
+START_TEST(test_prefix_triple_percent)
+{
+  char buffer[128];
+  const char *format = "hello % % %";
+  int actual_result = ft_printf(format);
+  int expected_result = 8;
+  ck_assert_pstr_eq(get_write_buf(), "hello % ");
+  ck_assert_int_eq(actual_result, expected_result);
+}
+END_TEST
+
 Suite *ft_printf_suite(void)
 {
   Suite *s;
@@ -4380,6 +4391,7 @@ Suite *ft_printf_suite(void)
   tcase_add_test(tc_error_type, test_prefix_percent);
   tcase_add_test(tc_error_type, test_prefix_suffix_percent);
   tcase_add_test(tc_error_type, test_prefix_double_percent);
+  tcase_add_test(tc_error_type, test_prefix_triple_percent);
   
   suite_add_tcase(s, tc_single_format_param);
   suite_add_tcase(s, tc_single_param);
