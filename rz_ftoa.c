3336c5029d42e6e56f0f548d044e41440c1b6216
Chip <lefrostx@gmail.com>
2019-10-17 15:26:18 +0500

    Added test for "%.19f", 1.025978542436587568678

--- rz_ftoa.c
+++ rz_ftoa.c
@@ -1,52 +1,89 @@
 #include "rz_printf.h"
 
-static unsigned long rz_modfl(long double n, int precision, long *i)
+static long rz_ext(long double n, int p, unsigned long *l, unsigned long *h)
 {
-    unsigned long frac;
-    unsigned long pre_pow;
+    long i;
+    unsigned long pl_pow;
+    unsigned long ph_pow;
+    int tp;
     long double tn;
 
-    *i = n;
-    n -= *i;
+    i = n;
+    n -= i;
+    *h = 0;
     if (n < 0)
 	n = -n;
-    if (precision > 18)
-	precision = 18;
-    pre_pow = rz_pow(10, precision);
-    n *= pre_pow;
-    frac = n;
-    tn = n - (unsigned long) n;
-    tn *= 10;
-    if (((unsigned long) tn) % 10 >= 5)
-	frac++;
-    if (frac >= pre_pow)
+    tp = p;
+    if (p > 18)
+	p = 18;
+    pl_pow = rz_pow(10, p);
+    n *= pl_pow;
+    *l = n;
+    n -= *l;
+    if (tp > 18)
     {
-	*i += rz_tern_l(*i > 0, 1, -1);
-	frac = 0;
+	p = tp - 18;
+	if (p > 18)
+	    p = 18;
+	ph_pow = rz_pow(10, p);
+	n *= ph_pow;
+	*h = n;
+	tn = n - (unsigned long) n;
+	tn *= 10;
+	if (((unsigned long) tn) % 10 >= 5)
+	    (*h)++;
+	if (*h >= ph_pow)
+	{
+	    *h = 0;
+	    (*l)++;
+	    if (*l >= pl_pow)
+	    {
+		i += rz_tern_l(i > 0, 1, -1);
+		*l = 0;
+	    }
+	}
     }
-    return (frac);
+    else
+    {
+	tn = n - (unsigned long) n;
+	tn *= 10;
+	if (((unsigned long) tn) % 10 >= 5)
+	    (*l)++;
+	if (*l >= pl_pow)
+	{
+	    i += rz_tern_l(i > 0, 1, -1);
+	    *l = 0;
+	    *h = 0;
+	}
+    }
+    return (i);
 }
 
-static int rz_ftoa_frac(char *buf, t_rz_arg *f, unsigned long frac)
+static int rz_ftoa_f(char *buf, t_rz_arg *f, unsigned long l, unsigned long h)
 {
-    char frep[21];
+    char lrep[21];
+    char hrep[21];
     int zero_prefix;
     int zero_suffix;
     int total_len;
-    int frac_len;
-    
+    int l_len;
+    int h_len;
+
     zero_prefix = 0;
-    frep[0] = '\0';
-    frac_len = rz_ultoa(frep, f, frac);
-    total_len = frac_len;
-    if (f->precision > frac_len)
+    lrep[0] = '\0';
+    hrep[0] = '\0';
+    l_len = rz_ultoa(lrep, f, l);
+    h_len = h > 0 ? rz_ultoa(hrep, f, h) : 0;
+    total_len = l_len + h_len;
+    if (f->precision > total_len)
     {
-	zero_prefix = f->precision - frac_len;
+	zero_prefix = f->precision - total_len;
 	rz_memset(buf, '0', zero_prefix);
 	total_len += zero_prefix;
     }
-    rz_memcpy(buf + zero_prefix, frep, frac_len);
-    zero_suffix = f->precision - frac_len - zero_prefix;
+    rz_memcpy(buf + zero_prefix, lrep, l_len);
+    rz_memcpy(buf + zero_prefix + l_len, hrep, h_len);
+    zero_suffix = f->precision - l_len - h_len - zero_prefix;
     if (zero_suffix > 0)
 	f->floatzero = zero_suffix;
     return (total_len);
@@ -55,17 +92,18 @@ static int rz_ftoa_frac(char *buf, t_rz_arg *f, unsigned long frac)
 int rz_ftoa(char *buf, t_rz_arg *f, long double n)
 {
     long integer;
-    unsigned long frac;
+    unsigned long l;
+    unsigned long h;
     int buf_len;
 
     if (n < 0)
 	f->negative = 1;
-    frac = rz_modfl(n, f->precision, &integer);
+    integer = rz_ext(n, f->precision, &l, &h);
     buf_len = rz_ltoa(buf, f, integer);
     if (f->precision > 0 || f->sharp)
 	buf[buf_len++] = '.';
     if (f->precision > 0)
-	buf_len += rz_ftoa_frac(buf + buf_len, f, frac);
+	buf_len += rz_ftoa_f(buf + buf_len, f, l, h);
     buf[buf_len] = '\0';
     return (buf_len);
 }
